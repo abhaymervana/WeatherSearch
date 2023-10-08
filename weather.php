00c8bb9f1877739dd194065bd4ca8ee67bbc9a83
abhaymervana <142957654+abhaymervana@users.noreply.github.com>
2023-10-08 11:00:31 -0400

    Add files via upload

---
+++ weather.php
@@ -0,0 +1,22 @@
+<?php
+
+$city = urlencode($_GET['city']);
+$source = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22".$_GET['city']."%2C%20%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $source);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_SSLVERSION,3);
+$data = curl_exec ($ch);
+$error = curl_error($ch); 
+curl_close ($ch);
+
+echo $data;
+if ($error) {
+    // Handle the error, log it, or display a user-friendly message
+    echo "Error: " . $error;
+} else {
+    // Proceed with processing the data
+    echo $data;
+}
+
+?>
